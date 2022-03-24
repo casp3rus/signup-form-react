6d6d859d21dc858e2a17ffd7995139b5626af0c0
Adrian M. Popan <adrian.popan@gmail.com>
2022-03-23 22:49:35 -0500

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const initialFormData = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+  isChecked: false
+};
+
+export default function App() {
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: type === 'checkbox' ? checked : value
+    }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    if (!formData.email || !formData.password || !formData.confirmPassword) {
+      alert('Please fill in all fields')
+    } else if (formData.password !== formData.confirmPassword) {
+      alert('Passwords to not match');
+    } else {
+      alert(`Successfully signed up. ${formData.isChecked ? 'Thanks for signing up for our newsletter!' : ''}`);
+    }
+    setFormData(initialFormData)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="form-container">
+      <form
+        className="form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form--input"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          className="form--input"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
 
-export default App;
+        <div className="form--marketing">
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            checked={formData.isChecked}
+            name="isChecked"
+            onChange={handleChange}
+
+          />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+        </div>
+        <button
+          className="form--submit"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  )
+}
